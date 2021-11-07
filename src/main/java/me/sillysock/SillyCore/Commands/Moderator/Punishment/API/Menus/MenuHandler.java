b41d415c56391beb46096b579e07da23d6d9a808
Silly Sock <sillysockcodes@gmail.com>
2021-11-07 13:48:52 +1030

    Backup

--- src/main/java/me/sillysock/SillyCore/Commands/Moderator/Punishment/API/Menus/MenuHandler.java
+++ src/main/java/me/sillysock/SillyCore/Commands/Moderator/Punishment/API/Menus/MenuHandler.java
@@ -1,6 +1,7 @@
 package me.sillysock.SillyCore.Commands.Moderator.Punishment.API.Menus;
 
 import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import me.sillysock.SillyCore.Commands.Moderator.Punishment.API.PunishmentType;
 import me.sillysock.SillyCore.SillyCore;
 import org.bukkit.entity.Player;
@@ -13,6 +14,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 
 import java.awt.*;
+import java.time.Instant;
 import java.util.Locale;
 
 public class MenuHandler implements Listener {
@@ -20,6 +22,20 @@ public class MenuHandler implements Listener {
     private final MenuApi api = SillyCore.getMenuApi();
     private final BiMap<Player, PunishmentType> punishmentTypeBiMap = MenuApi.getPunishmentTypeBiMap();
     private final BiMap<Player, OfflinePlayer> toPunish = MenuApi.getToPunish();
+
+    private static final int SECOND = 1;
+    private static final int MINUTE = SECOND * 60;
+    private static final int HOUR = MINUTE * 60;
+    private static final int DAY = HOUR * 24;
+    private static final long WEEK = DAY * 7;
+    private static final long MONTH = WEEK * 30;
+    private static final long YEAR = MONTH * 12;
+    private static final int PERMANENT = -1;
+    private long currTime;
+    private long EXPIRY;
+
+    private OfflinePlayer target;
+    private Player targetPlayer;
 
     @EventHandler
     public void MuteMenuHandler(final InventoryClickEvent e) {
@@ -32,13 +48,30 @@ public class MenuHandler implements Listener {
         e.setCancelled(true);
 
         if (!punishmentTypeBiMap.containsKey(p)) return;
-        final String name = item.getItemMeta().getDisplayName().toLowerCase(Locale.ROOT);
+        final String name = item.getItemMeta().getDisplayName().toLowerCase(Locale.ENGLISH);
 
-        if (name.contains("minute")) p.sendMessage("minute");
-        if (name.contains("permanent")) p.sendMessage("permanent");
-        if (name.contains("year")) p.sendMessage("year");
-        if (name.contains("month")) p.sendMessage("month");
-        if (name.contains("day")) p.sendMessage("day");
-        if (name.contains("week")) p.sendMessage("week");
+        target = toPunish.get(p);
+        targetPlayer = target.getPlayer();
+        currTime = Instant.now().getEpochSecond();
+
+        if (name.contains("minute")) {
+            EXPIRY = currTime + MINUTE;
+            if (target.isOnline()) {
+                targetPlayer.sendMessage();
+            }
+        } else if (name.contains("permanent")) {
+            EXPIRY = -1;
+        } else if (name.contains("year")) {
+            EXPIRY = currTime + YEAR;
+        } else if (name.contains("month")) {
+            EXPIRY = currTime + MONTH;
+        } else if (name.contains("day")) {
+            EXPIRY = currTime + DAY;
+        } else if (name.contains("week")) {
+            EXPIRY = currTime + WEEK;
+        } else if (name.contains("hour")) {
+            EXPIRY = currTime + HOUR;
+        }
+
     }
 }
