63b90680a6463fa66759a074fc8621e91819b0cb
Silly Sock <sillysockcodes@gmail.com>
2021-11-17 19:22:25 +1030

    fun command

--- src/main/java/me/sillysock/SillyCore/API/Configuration/Config.java
+++ src/main/java/me/sillysock/SillyCore/API/Configuration/Config.java
@@ -3,6 +3,8 @@ package me.sillysock.SillyCore.API.Configuration;
 import me.sillysock.SillyCore.SillyCore;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Config {
@@ -12,16 +14,24 @@ public final class Config {
     private static List<String> nicknameBadWords;
     private static String storage;
     private static boolean opDisabled;
+    private static int nicknameMaxLength;
 
-    public static void setValues() {
+    public static void setValues() throws IOException {
         config = SillyCore.getInstance().getConfig();
 
         setNicknameBadWords(config.getStringList("nickname_bad_words"));
         storage = config.getString("storage");
         opDisabled = config.getBoolean("disable_op");
+        nicknameMaxLength = config.getInt("nickname_max_length");
 
         Lang.setValues();
         Permissions.setValues();
+    }
+
+    public static void checkConfigValues() {
+        if (config.getStringList("nickname_bad_words") == null) config.set("nickname_bad_words", new ArrayList<String>());
+        if (config.getString("storage") == null) config.set("storage", "yml");
+        if (config.getBoolean("disable_op") == false) config.set("disable_op", false);
     }
 
     public static void clear() {
---
+++ src/main/java/me/sillysock/SillyCore/Commands/Miscellaneous/Fun/FunCommand.java
@@ -0,0 +1,46 @@
+package me.sillysock.SillyCore.Commands.Miscellaneous.Fun;
+
+import me.sillysock.SillyCore.API.Configuration.Lang;
+import me.sillysock.SillyCore.API.Util.MessageUtils;
+import me.sillysock.SillyCore.SillyCore;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class FunCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command cmd,
+                             final String label, final String[] args) {
+
+        if (!(sender instanceof Player p)) {
+            SillyCore.getLog().severe("Only players may execute this command.");
+            return true;
+        }
+
+        if (!p.hasPermission("sillycore.misc.fun")) {
+            p.sendMessage(Lang.getNoPermission());
+            return true;
+        }
+
+        if (args.length == 0 || args.length == 1 && !args[1].equalsIgnoreCase("confirm")) {
+            p.sendMessage(MessageUtils.format("&cUsage: /fun confirm"));
+            return true;
+        }
+
+        final World world = p.getWorld();
+        final Location loc = p.getLocation();
+        for (int i = 0; i < 100; i++) {
+            world.spawnEntity(loc, EntityType.LIGHTNING);
+        }
+
+        p.sendMessage(MessageUtils.format("&cThou hast been smiten!"));
+
+        return false;
+    }
+}
